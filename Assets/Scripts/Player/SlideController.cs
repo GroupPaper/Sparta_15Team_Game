411e22b8b33f288079cfd307f7edabc065676a1e
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-05-09 14:36:39 +0900

    feat : slide , sprite 추가

---
+++ Assets/Scripts/Player/SlideController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideController : MonoBehaviour
+{
+    private GroundChecker _groundChecker;
+    private JumpController _jumpController;
+
+    private bool isSliding;
+}
