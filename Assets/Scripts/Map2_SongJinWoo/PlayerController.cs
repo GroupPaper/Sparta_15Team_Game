6f012043a43085eff8459a1f463bb2acd08ae4c3
GroupPaper <thdwlsdnww@naver.com>
2025-05-12 11:28:50 +0900

    feat_song_테스트 플레이어 추가, 테스트 카메라 추가, 맵 수정
    테스트 플레이어 추가, 테스트 카메라 추가, 맵 수정

---
+++ Assets/Scripts/Map2_SongJinWoo/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject body; // Body 오브젝트 (애니메이션용)
+    public float moveSpeed = 3f;
+    public float jumpForce = 5f;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private int jumpCount = 0;
+    private bool isGrounded = false;
+    private bool isSliding = false; // 슬라이드 여부
+
+    void Start()
+    {
+        animator = body.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 항상 오른쪽으로 이동
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        // 점프 입력 처리
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                jumpCount = 1;
+                isGrounded = false;
+            }
+            else if (jumpCount == 1)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                jumpCount = 2;
+            }
+        }
+
+        // 슬라이드 입력 처리
+        if (Input.GetKey(KeyCode.LeftShift)) // 슬라이드 키를 누르고 있을 때
+        {
+            isSliding = true;
+            animator.SetBool("isSliding", true); // 슬라이드 애니메이션 재생
+        }
+        else
+        {
+            isSliding = false;
+            animator.SetBool("isSliding", false); // 슬라이드 애니메이션 멈춤
+        }
+
+        // 슬라이딩 상태에서의 이동 처리 (이동 속도 적용)
+        if (isSliding)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime); // 이동 속도 그대로 사용
+        }
+
+        // 애니메이션 처리 (필요시)
+        animator.SetInteger("jumpCount", jumpCount);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f) // 바닥과 충돌
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
+}
